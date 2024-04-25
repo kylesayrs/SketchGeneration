9e4d219c040013baf74b8cb6695ca1bffba0ef20
Kyle Sayers <kylesayrs@gmail.com>
2024-04-24 23:08:24 -0400

    wow, actually robust squares, nocap

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel, Field, model_validator
 
 
 class ModelConfig(BaseModel):
-    embed_dims: int = Field(default=32)
-    hidden_dims: int = Field(default=64)
+    embed_dims: int = Field(default=128)
+    hidden_dims: int = Field(default=256)
     num_heads: int = Field(default=1)
 
     max_sequence_length: int = Field(default=50)
 
-    num_layers: int = Field(default=2)
+    num_layers: int = Field(default=5)
     dropout: float = Field(default=0.1)
     num_components: int = Field(default=3)
     elu_alpha: float = Field(default=1.0)
@@ -22,10 +22,10 @@ class TrainingConfig(BaseModel):
     batch_size: int = Field(default=128)
     max_sequence_length: int = Field(default=50)
     data_sparsity: int = Field(default=1)
-    aug_scale_factor: float = Field(default=0.0)
+    aug_scale_factor: float = Field(default=0.05)
 
     # optimizer
-    learning_rate: float = Field(default=5e-06)
+    learning_rate: float = Field(default=1e-04)
     gradient_clip: Union[float, None] = Field(default=None)
 
     # logging
